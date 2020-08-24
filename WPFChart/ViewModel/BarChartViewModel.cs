57e51924b3e02cc15de12df9653946f6291cf6b6
Leoyang <leoyang@jepun.com.tw>
2020-08-24 16:12:44 +0800

    first version commit

---
+++ WPFChart/ViewModel/BarChartViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace WPFChart.ViewModel
+{
+    public class BarChartViewModel:ViewModelBase
+    {
+        private List<DataModel> _DataCollection = new List<DataModel>();
+
+        public BarChartViewModel()
+        {
+            DataCollection.Add(new DataModel(){ Key = "C#", Value = 50});
+            DataCollection.Add(new DataModel(){ Key = "Python", Value = 20});
+            DataCollection.Add(new DataModel(){ Key = "SQL", Value = 30});
+        }
+
+        public List<DataModel> DataCollection
+        {
+            get
+            {
+                return _DataCollection;
+            }
+            set
+            {
+                _DataCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+
+    public class DataModel
+    {
+        public string Key { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ WPFChart/ViewModel/CartesianChartViewModel.cs
@@ -0,0 +1,43 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace WPFChart.ViewModel
+{
+    public class CartesianChartViewModel:ViewModelBase
+    {
+        #region Declarations
+        private SeriesCollection _Data;
+        #endregion
+
+        #region Property
+        public SeriesCollection Data
+        {
+            get
+            {
+                return _Data;
+            }
+            set
+            {
+                _Data = value;
+            }
+        }
+        #endregion
+
+        #region Memberfunction
+        public CartesianChartViewModel()
+        {
+            Data = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<double> { 3, 5, 7, 4 }
+                },
+                new ColumnSeries                
+                {
+                    Values = new ChartValues<decimal> { 5, 6, 2, 7 }
+                }
+            };
+        }
+        #endregion
+    }
+}
